03af13ebfeae2d90932d57e3b62dc3345e7270f6
SANJAY-KUMAR-7 <83998914+SANJAY-KUMAR-7@users.noreply.github.com>
2021-07-02 14:24:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function (){
+
+
+var typed = new Typed(".typing", {
+    strings: ["Developer.","Designer.", "Freelancer."],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+});
